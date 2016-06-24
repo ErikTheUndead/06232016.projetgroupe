719736894307e76166724f1e6bab799c093aa9c1
inti0335 <inti0335@INTI-0335>
2016-06-24 11:27:59 +0200

    ajout du dragon

---
+++ 06232016.projetgroupe/src/com/myapp/entities/Dragon.java
@@ -0,0 +1,13 @@
+package com.myapp.entities;
+
+public class Dragon extends Monstre{
+
+	public Dragon() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void sortDeFeu(){
+		System.out.println("!!! Le feu du dragon !!!");
+	}
+}
---
+++ 06232016.projetgroupe/src/com/myapp/junit/Test.java
@@ -0,0 +1,12 @@
+package com.myapp.junit;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
